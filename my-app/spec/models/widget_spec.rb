1346cf95882241267e2ccba7dbccaecb9688ee99
Tarun Garg <tarun.garg@berylsystems.com>
2018-07-23 18:11:47 +0530

    test with subject method

--- my-app/spec/models/widget_spec.rb
+++ my-app/spec/models/widget_spec.rb
@@ -1,35 +1,20 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe Auction, :type => :model do
-  subject { described_class.new }
+# RSpec.describe Widget, type: :model do
+#   widget = Widget.new
 
-  it "is valid with valid attributes" do
-    subject.title = "Anything"
-    subject.description = "Anything"
-    subject.start_date = DateTime.now
-    subject.end_date = DateTime.now + 1.week
-    expect(subject).to be_valid
-  end
+#   it "is valid with valid attributes" do
+    
+#     expect(widget).to be_valid
+#   end  
 
-  it "is not valid without a title" do
-    expect(subject).to_not be_valid
-  end
+#   it "is not valid without a name" do
+#     widget.name='sdf'
+#     expect(widget).to be_valid
+#   end
 
-  it "is not valid without a description" do
-    subject.title = "Anything"
-    expect(subject).to_not be_valid
-  end
+#   it "must contain date" do
+#     expect(widget.date = nil).to be_valid
+#   end
 
-  it "is not valid without a start_date" do
-    subject.title = "Anything"
-    subject.description = "Lorem ipsum dolor sit amet"
-    expect(subject).to_not be_valid
-  end
-
-  it "is not valid without a end_date" do
-    subject.title = "Anything"
-    subject.description = "Lorem ipsum dolor sit amet"
-    subject.start_date = DateTime.now
-    expect(subject).to_not be_valid
-  end
-end
+# end
