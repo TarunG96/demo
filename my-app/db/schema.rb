7c9f97b8563d11a38e65252b2a4ae43ce3345b56
Tarun Garg <tarun.garg@berylsystems.com>
2018-07-23 18:17:00 +0530

    new model for testing

--- my-app/db/schema.rb
+++ my-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_23_123411) do
+ActiveRecord::Schema.define(version: 2018_07_23_124407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2018_07_23_123411) do
     t.datetime "end_date"
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "pass"
+    t.string "email"
+    t.integer "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
